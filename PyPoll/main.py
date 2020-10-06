ba90c7d5f0c8de98c6fc23adbf7e04609a456d3e
marinahobday <70069406+marinahobday@users.noreply.github.com>
2020-10-05 21:31:09 -0400

    Final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,12 +16,12 @@ with open(pypoll_csv) as csvfile:
     #header set
     header = next(csvreader)
 
-    #starting our first loop to find total
+    #starting our first loop to find total voters
     for row in csvreader:
         total += 1
 
         #looping through names to find the candidates and count how many votes they received to put into a list
-        # they lists are: unique_candidates and vote_counts
+        # the lists are: unique_candidates and vote_counts
         if row[2] not in unique_candidates:
             unique_candidates.append(row[2])
             index = unique_candidates.index(row[2])
@@ -33,8 +33,7 @@ with open(pypoll_csv) as csvfile:
     #outside of first loop, new loop to find the percentage and put it in a list called vote_percent
     for votes in vote_counts:
         percent = (votes/total) * 100
-        percent = round(percent)
-        percent = "%.3f%%" % percent
+        percent = str(round(percent, 2))
         vote_percent.append(percent)
 
     #outside of all loops, now must find winner through the one with the max vote counts
@@ -48,10 +47,10 @@ print("Election Results")
 print("-------------------")
 print(f"Total Votes: {total}")
 print("-------------------")
-print(f"{unique_candidates[0]}: {str(vote_percent[0])} ({str(vote_counts[0])})")
-print(f"{unique_candidates[1]}: {str(vote_percent[1])} ({str(vote_counts[1])})")
-print(f"{unique_candidates[2]}: {str(vote_percent[2])} ({str(vote_counts[2])})")
-print(f"{unique_candidates[3]}: {str(vote_percent[3])} ({str(vote_counts[3])})")
+print(f"{unique_candidates[0]}: {str(vote_percent[0])}% ({str(vote_counts[0])})")
+print(f"{unique_candidates[1]}: {str(vote_percent[1])}% ({str(vote_counts[1])})")
+print(f"{unique_candidates[2]}: {str(vote_percent[2])}% ({str(vote_counts[2])})")
+print(f"{unique_candidates[3]}: {str(vote_percent[3])}% ({str(vote_counts[3])})")
 print("-------------------")
 print(f"Winner: {winner}")
 print("-------------------")
@@ -66,13 +65,13 @@ with open(pypoll_output, "a") as txt_file:
     txt_file.write("\n")   
     txt_file.write("-------------------")
     txt_file.write("\n")   
-    txt_file.write(unique_candidates[0] + ": " + str(vote_percent[0]) + " (" + str(vote_counts[0]) + ")")
+    txt_file.write(unique_candidates[0] + ": " + str(vote_percent[0]) + "% (" + str(vote_counts[0]) + ")")
     txt_file.write("\n")
-    txt_file.write(unique_candidates[1] + ": " + str(vote_percent[1]) + " (" + str(vote_counts[1]) + ")")
+    txt_file.write(unique_candidates[1] + ": " + str(vote_percent[1]) + "% (" + str(vote_counts[1]) + ")")
     txt_file.write("\n")
-    txt_file.write(unique_candidates[2] + ": " + str(vote_percent[2]) + " (" + str(vote_counts[2]) + ")")
+    txt_file.write(unique_candidates[2] + ": " + str(vote_percent[2]) + "% (" + str(vote_counts[2]) + ")")
     txt_file.write("\n")
-    txt_file.write(unique_candidates[3] + ": " + str(vote_percent[3]) + " (" + str(vote_counts[3]) + ")")
+    txt_file.write(unique_candidates[3] + ": " + str(vote_percent[3]) + "% (" + str(vote_counts[3]) + ")")
     txt_file.write("\n")   
     txt_file.write("-------------------")
     txt_file.write("\n")   
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-challenge
+Hello!
+Welcome to my Python Homework~
+Grade me nicely :)
